eeda6baedfef8a4c12d4e7ac191829ccf4a8beac
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-17 06:12:47 +0200

    add checkbox functionality

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,17 +1,35 @@
-import { useSelector } from "react-redux";
+import {
+     useDispatch,
+     useSelector } from "react-redux";
 import { RootState } from "../RTK/store"; // Adjust the import path as necessary
+import { checked } from "../RTK/todoSlice";
 
 const Items = () => {
     const item = useSelector((state: RootState) => state.todo.list);
+const dispatch = useDispatch();
+
+    const handlecheck = (e:number) => {
+        // console.log("checked");
+        dispatch(checked(e));
+    };
+
     return ( 
     <>
     {
         item.map((item, index) => {
             return (
-                <div key={index}>
-                    <h1>{item.title}</h1>
-                    <p>{item.done ? "Done" : "Not Done"}</p>
-                </div>
+                <div key={index} 
+                className="flex justify-between items-center border-b-2 border-pink-500 p-2"
+                >
+                    <label className={`${item.done?"line-through":""} text-pink-700`}>
+                        <input type="checkbox" name="" id="" 
+                        onClick={()=>handlecheck(item.id)}
+                        />
+                        {item.title}
+                    </label>
+               
+                 
+                  </div>
             );
     
     })
