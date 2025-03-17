a1f71c5ecd5ed06055207e7da0a7e7c16949b3d7
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-17 06:27:49 +0200

    modif Items check after delete prev note

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,45 +1,48 @@
-import {
-     useDispatch,
-     useSelector } from "react-redux";
-import { RootState } from "../RTK/store"; // Adjust the import path as necessary
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../RTK/store";
 import { checked, deleteo } from "../RTK/todoSlice";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Items = () => {
-    const item = useSelector((state: RootState) => state.todo.list);
-const dispatch = useDispatch();
+  const items = useSelector((state: RootState) => state.todo.list);
+  const dispatch = useDispatch();
 
-    const handlecheck = (e:number) => {
-        // console.log("checked");
-        dispatch(checked(e));
-    };
+  const handleCheck = (id: number) => {
+    dispatch(checked(id));
+  };
 
-    const handledelet = (e:number) => {
-       dispatch(deleteo(e));
-    };
+  const handleDelete = (id: number) => {
+    dispatch(deleteo(id));
+  };
 
-    return ( 
+  return (
     <>
-    {
-        item.map((item, index) => {
-            return (
-                <div key={index} 
-                className="flex justify-between items-center border-b-2 border-pink-500 p-2"
-                >
-                    <label className={`${item.done ? "line-through" : ""} text-pink-700`}>
-                        <input type="checkbox" 
-                        className="mr-2"
-                        onClick={() => handlecheck(item.id)}
-                        />
-                        {item.title}
-                    </label>
-                    <AiOutlineClose className="text-pink-700 bg-slate-200 rounded-full cursor-pointer" onClick={()=>handledelet(item.id)}/>
-                </div>
-            );
-    
-    })
-    }
-    </> );
-}
- 
+      {items.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="flex justify-between items-center border-b-2 border-pink-500 p-2"
+          >
+            <label
+              className={`${item.done ? "line-through" : ""} text-pink-700`}
+            >
+              <input
+                type="checkbox"
+                className="mr-2"
+                checked={item.done}
+                onChange={() => handleCheck(item.id)}
+              />
+              {item.title}
+            </label>
+            <AiOutlineClose
+              className="text-pink-700 bg-slate-200 rounded-full cursor-pointer"
+              onClick={() => handleDelete(item.id)}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
 export default Items;
